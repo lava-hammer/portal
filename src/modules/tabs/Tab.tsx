239b544e6c163ea07e9382c6f8bad13afd4c0197
hammer <lava_hammer@163.com>
2021-03-28 23:59:17 +0800

    + tab passing children props

--- src/modules/tabs/Tab.tsx
+++ src/modules/tabs/Tab.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { DOMElement, useState } from 'react'
 import classNames from 'classnames/bind'
 import { Playground } from '../playground/Playground'
 import tabStyle from './Tab.module.less'
@@ -6,7 +6,8 @@ import tabStyle from './Tab.module.less'
 const styles = classNames.bind(tabStyle)
 
 export interface ITabProp {
-  value: boolean,
+  value: string,
+  checked?: boolean,
   children?: React.ReactNode,
 }
 
@@ -15,8 +16,9 @@ export function Tab(prop: ITabProp) {
     <div 
       className={styles({
         tab: true,
-        active: prop.value,
+        active: prop.checked,
       })}
+      data-value={prop.value}
     >
       {prop.children}
     </div>
@@ -25,16 +27,35 @@ export function Tab(prop: ITabProp) {
 
 export interface ITabListProp {
   children?: React.ReactNode,
+  value?: string,
+  onChange?: (key: string) => void;
 }
 
 export function TabList(prop: ITabListProp) {
+  const [currValue, setValue] = useState<string>('')
+  const childrenWithProps = React.Children.map(prop.children, e => {
+    if (
+      React.isValidElement(e) && 
+      typeof e.type !== 'string' &&
+      e.type.name === 'Tab'
+    ) {
+      return React.cloneElement(e, {
+        checked: currValue === e.props.value,
+        ...e.props
+      })
+    }
+    return e
+  })
   return (
     <div
       onClick={(e) => {
-        console.log(e.target)
+        const newVal = (e.target as HTMLDivElement).dataset.value;
+        if (newVal) {
+          setValue(newVal)
+        }
       }}
     >
-      {prop.children}
+      {childrenWithProps}
     </div>
   )
 }
@@ -46,9 +67,9 @@ export function TestTab() {
       height={300}
     >
       <TabList>
-        <Tab>Home</Tab>
-        <Tab>Open Source</Tab>
-        <Tab>About</Tab>
+        <Tab value="home">Home</Tab>
+        <Tab value="open-source">Open Source</Tab>
+        <Tab value="about">About</Tab>
       </TabList>
     </Playground>
   )
